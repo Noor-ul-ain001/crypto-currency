02337882a685187e0379f49e2b4d303623d40fb0
Noor-ul-ain001 <noor01mk@gmail.com>
2025-06-24 18:06:12 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, RunConfig, function_tool
+from dotenv import load_dotenv
+import os
+import requests
+
+
+load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY is not set in .env file.")
+
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+@function_tool
+def get_crypto_price(symbol: str) -> str:
+    """
+    Fetch the current price of a cryptocurrency from Binance.
+    Example: BTCUSDT, ETHUSDT
+    """
+    url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol.upper()}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        price = response.json()["price"]
+        return f"The current price of {symbol.upper()} is ${price}"
+    else:
+        return "Invalid symbol or data fetch failed."
+
+
+crypto_agent = Agent(
+    name="CryptoDataAgent",
+    instructions="You provide real-time cryptocurrency prices using Binance API.",
+    tools=[get_crypto_price]
+)
+
+
+response = Runner.run_sync(
+    crypto_agent,
+    input="What is the current price of BTCUSDT?",
+    run_config=config
+)
+
+print(response.final_output)
